c568b1f8201a11a3d31e31364f6da6ada1ddd659
Cristian Rodier <crodier@protonmail.com>
2025-05-08 15:33:56 -0700

    I fixed several bugs in test_cubeit

---
+++ tests/test_cubeit.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import Page, expect
+from random import randint
+
+BASE_URL = "http://localhost:8000/"
+
+def test_cube(page: Page):
+    page.goto(BASE_URL)
+
+    number_input = page.locator('#numberInput')
+    random_int = randint(1, 100)
+    
+    number_input.fill(str(random_int))
+
+    page.locator("#cubeButton").click()
+
+    result = page.locator('#result')
+
+    expect(result).to_contain_text(f"{random_int}^3 = {random_int**3}")
+
+
+def test_blank_input(page: Page):
+    page.goto(BASE_URL)
+
+    page.locator("#cubeButton").click()
+
+    result = page.locator('#result')
+
+    expect(result).to_contain_text(f"Put in something")
+
